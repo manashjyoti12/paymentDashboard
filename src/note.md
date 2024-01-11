bed0276e5ddb8fbc091b651880ac2047593e4a80
Manashjyoti Boro <manashjyoti533@gmail.com>
2024-01-11 13:41:53 +0530

    update sidebar

--- src/note.md
+++ src/note.md
@@ -1,3 +1,5 @@
 for map function look =>{
     https://github.com/Sridhar-C-25/react-tailwind-sidebar/blob/main/src/pages/Home.jsx
 }
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { IoIosArrowBack } from "react-icons/io";
 import { GoHome } from "react-icons/go";
+import { LuClipboardList } from "react-icons/lu";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { BsTruck } from "react-icons/bs";
 
 const Home = () => {
   const [open, setOpen] = useState(true);
 
+  const menus = [
+    { name: "Home", link: "/", icon: GoHome, active: true },
+    { name: "Orders", link: "/", icon: LuClipboardList },
+    { name: "Products", link: "/", icon: HiOutlineSquares2X2 },
+    { name: "Delivery", link: "/", icon: BsTruck },
+  ];
+
   return (
     <div className="home">
+      {/* SIDEBAR */}
       <div
         className={`sidebar h-screen bg-[#1E2640] text-[#CED0D5] p-4 ${
-          open ? "w-52" : "w-24"
+          open ? "w-60" : "w-24"
         } duration-300 relative`}
       >
         <IoIosArrowBack
@@ -22,7 +33,8 @@ const Home = () => {
           }}
         />
 
-        <div className="HEAD flex gap-3 ">
+        {/* SIDEBAR HEAD SECTION */}
+        <div className="HEAD mb-10 flex gap-3 ">
           <img
             src="https://png.pngtree.com/png-vector/20190322/ourmid/pngtree-shop-logo-vector-template-design-illustration-png-image_860078.jpg"
             className="w-14 rounded-lg"
@@ -40,18 +52,33 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="BODY mt-10 flex justify-center">
-          <div className="flex items-center  gap-4 cursor-pointer">
-            <GoHome className="text-3xl" />
-            <span
-              className={`transition ease-in-out duration-300 ${
-                !open && "hidden"
-              }`}
-            >
-              Home
-            </span>
-          </div>
-        </div>
+        {/* SIDEBAR BODY SECTION */}
+        {menus?.map((menu) => (
+          <>
+            <div className="BODY pl-3  mt-4 flex justify-center">
+              <div
+                className={`flex items-center gap-3 w-full cursor-pointer p-2 rounded-lg ${
+                  menu.active ? "bg-[#343C53] " : "bg-[#1E2640]"
+                }`}
+              >
+                <div
+                  className={
+                    menu.active ? "text-[#FEFEFE] " : "text-[#CED0D5] "
+                  }
+                >
+                  {React.createElement(menu?.icon, { size: "30" })}
+                </div>
+                <span
+                  className={`transition ease-in-out duration-300 ${
+                    !open && "hidden"
+                  } ${menu.active ? "text-[#FEFEFE] " : "text-[#CED0D5] "}`}
+                >
+                  {menu.name}
+                </span>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
 
       {/* NAVBAR*/}
